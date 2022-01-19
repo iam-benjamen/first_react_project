ea8d0cb25e9110cef862d94090bc389ba6512dd1
Areo Bnejamen <areotimileyin1@gmail.com>
2022-01-19 18:15:33 +0100

    Routing and useFetch Implemented

---
+++ src/ClassComponent.js
@@ -0,0 +1,15 @@
+//Practicing class components
+import React from "react";
+
+class MyComponent extends React.Component {
+
+    constructor(props){
+        super(props);
+    }
+
+    render() { 
+        return <div>Hello, Tech Bros!</div>;
+    }
+}
+ 
+export default MyComponent;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
@@ -5,8 +7,8 @@ const Navbar = () => {
                 The Dojo Blog
             </h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
